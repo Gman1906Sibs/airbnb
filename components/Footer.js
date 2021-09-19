e834e0d50566427ce17b654983ab1215d1f29182
Gman1906Sibs <studio1906mail@gmail.com>
2021-09-19 17:39:27 +0200

    home page

---
+++ components/Footer.js
@@ -0,0 +1,44 @@
+function Footer() {
+    return (
+        <div className=" grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600 ">
+            <div className=" space-y-4 text-xs text-gray-800 ">
+                <h5 className=" font-bold ">ABOUT</h5>
+                <p>Accessibility</p>
+                <p>This is not a real site</p>
+                <p>Its a pretty awesome clone</p>
+                <p>AReferrals accepted</p>
+                <p> Clone by Gman Sibs</p>
+            </div>
+
+            <div className=" space-y-4 text-xs text-gray-800 ">
+                <h5 className=" font-bold ">COMMUNITY</h5>
+                <p>Gman Sibs</p>
+                <p>Junior</p>
+                <p>React</p>
+                <p>Developer</p>
+                <p>Portfolio project</p>
+            </div>
+
+            <div className=" space-y-4 text-xs text-gray-800 ">
+                <h5 className=" font-bold ">HOST</h5>
+                <p>Someone</p>
+                <p>Please</p>
+                <p>Hire</p>
+                <p>Gman Sibs</p>
+                <p>Thank you</p>
+            </div>
+
+            <div className=" space-y-4 text-xs text-gray-800 ">
+                <h5 className=" font-bold ">SUPPORT</h5>
+                <p>Contact Gman Sibs</p>
+                <p>+27 61 044 5723</p>
+                <p>gmansibs@gmail.com</p>
+                <p>studio1906mail@gmail.com</p>
+                <p>Give me a shout</p>
+            </div>
+
+        </div>
+    )
+}
+
+export default Footer
